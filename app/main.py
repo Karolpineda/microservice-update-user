1244874cfa9cbe5c91c473434d4e36e7e8d1648e
Karolpineda <karolpineda@hotmail.com>
2025-02-05 02:22:52 -0500

    feat: :sparkles: Update main

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
-
-
+# app/main.py
 from fastapi import FastAPI
-from app.controllers import user  # Solo importa el controlador de actualización
-from app.docs import swagger_router  # Importar swagger_router
+from app.controllers import user  # Importa el controlador de usuario
+from app.database import engine
+from app.models import Base
 
-app = FastAPI(
-    title="API de actualización de usuarios",
-    description="Microservicio para la actualización de usuarios",
-    version="1.0.0",
-)
+app = FastAPI()
 
-# Incluir las rutas de actualización de usuarios
+# Crear las tablas en la base de datos
+Base.metadata.create_all(bind=engine)
+
+# Incluir las rutas de usuario
 app.include_router(user.router)
