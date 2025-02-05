cb2a6aa753d4ee16078028a3b3ffa9a9ca6f359b
Karolpineda <karolpineda@hotmail.com>
2025-02-05 02:23:17 -0500

    feat: :sparkles: Update controllers for update user

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -1,20 +1,20 @@
+# app/controllers/user.py
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app import schemas, database  # Importaciones generales
-from app.services.user import update_user  # Importación desde services/user.py
+from app import schemas, database
+from app.services.user import update_user, get_user_by_id
+
 
 router = APIRouter()
 
-# Ruta Health para verificar si el microservicio está activo
-@router.get("/health")
-def health_check():
-    return {"status": "Microservice Users (Update) is up and running"}
-
 # Ruta para actualizar un usuario
 @router.put("/users/{user_id}", response_model=schemas.UserOut)
-def update_user_route(user_id: str, user: schemas.UserCreate, db: Session = Depends(database.get_db)):
-    db_user = get_user_by_email(db, email=user.email)
-    if db_user and db_user.id != user_id:
-        raise HTTPException(status_code=400, detail="Email already registered")
+def update_user_route(user_id: str, user: schemas.UserUpdate, db: Session = Depends(database.get_db)):
+    # Verificar si el usuario existe
+    db_user = get_user_by_id(db, user_id=user_id)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
     
+    # Actualizar los campos
     return update_user(db=db, user_id=user_id, user=user)
+
