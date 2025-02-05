091eaabe341ab4435e6bfca52360965e11059c70
Karolpineda <karolpineda@hotmail.com>
2025-02-04 21:16:07 -0500

    feat: :construction: add schemas of microserves

---
+++ app/schemas/user.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from uuid import UUID
+
+class UserBase(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class UserOut(UserBase):
+    id: UUID
+    
+    class Config:
+        orm_mode = True  # Esto permite que Pydantic trabaje con los objetos de SQLAlchemy
