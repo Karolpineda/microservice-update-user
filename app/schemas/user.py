7fbb8173606c461af7672c3acb76ac949ce54e62
Karolpineda <karolpineda@hotmail.com>
2025-02-04 21:37:16 -0500

    feat: :hammer: update schema of microservice

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,16 +1,17 @@
 from pydantic import BaseModel
 from uuid import UUID
+from typing import Optional
 
 class UserBase(BaseModel):
     first_name: str
     last_name: str
     email: str
 
-class UserCreate(UserBase):
-    password: str
+class UserUpdate(UserBase):
+    password: Optional[str] = None  # Si deseas permitir la actualización de la contraseña
 
 class UserOut(UserBase):
     id: UUID
-    
+
     class Config:
         orm_mode = True  # Esto permite que Pydantic trabaje con los objetos de SQLAlchemy
