e8c1d685ac7fe9b05d19e60def9af1eddc4225eb
Karolpineda <karolpineda@hotmail.com>
2025-02-05 02:21:58 -0500

    feat: :art: Update for schema

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -2,14 +2,24 @@ from pydantic import BaseModel
 from uuid import UUID
 from typing import Optional
 
+# app/schemas/user.py
+from pydantic import BaseModel
+
 class UserBase(BaseModel):
     first_name: str
     last_name: str
     email: str
+    password: str
 
+    class Config:
+        orm_mode = True
+
+
+# Esquema para la actualización de un usuario
 class UserUpdate(UserBase):
     password: Optional[str] = None  # Si deseas permitir la actualización de la contraseña
 
+# Esquema para los datos del usuario que se devolverán
 class UserOut(UserBase):
     id: UUID
 
