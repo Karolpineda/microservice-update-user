6a8bbfe8204f150113cfd77925c5686e91cc8a85
Karolpineda <karolpineda@hotmail.com>
2025-02-04 21:38:54 -0500

    feat: :hammer: Update main

--- app/main.py
+++ app/main.py
@@ -1,21 +1,14 @@
-# app/main.py
+
+
 from fastapi import FastAPI
-from app.controllers import user  # Rutas de la aplicación
-from app.database import engine
-from app.models import Base
+from app.controllers import user  # Solo importa el controlador de actualización
 from app.docs import swagger_router  # Importar swagger_router
 
 app = FastAPI(
-    title="Mi API",
-    description="Documentación personalizada de mi API",
+    title="API de actualización de usuarios",
+    description="Microservicio para la actualización de usuarios",
     version="1.0.0",
-    docs_url=None,    # Deshabilita /docs por defecto
-    redoc_url=None    # Deshabilita /redoc
 )
 
-# Crear las tablas en la base de datos
-Base.metadata.create_all(bind=engine)
-
-# Incluir las rutas de la aplicación
+# Incluir las rutas de actualización de usuarios
 app.include_router(user.router)
-app.include_router(swagger_router)  # Incluir el router de Swagger
