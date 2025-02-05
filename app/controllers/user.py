596a8cb9a2e8e1665b625a96bcbfa38a84c4689b
Karolpineda <karolpineda@hotmail.com>
2025-02-04 21:34:25 -0500

    feat: :hammer: Update for update user

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -1,31 +1,20 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app import schemas, database  # Importaciones generales
-from app.schemas.user import UserBase, UserCreate, UserOut
-from app.services.user import get_user_by_email, create_user, verify_password  # Importaciones desde services/user.py
+from app.services.user import update_user  # Importación desde services/user.py
 
 router = APIRouter()
 
 # Ruta Health para verificar si el microservicio está activo
 @router.get("/health")
 def health_check():
-    return {"status": "Microservice Users is up and running"}
+    return {"status": "Microservice Users (Update) is up and running"}
 
-@router.post("/users/", response_model=schemas.UserOut)  # Aquí se utiliza UserOut
-def create_user_route(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
+# Ruta para actualizar un usuario
+@router.put("/users/{user_id}", response_model=schemas.UserOut)
+def update_user_route(user_id: str, user: schemas.UserCreate, db: Session = Depends(database.get_db)):
     db_user = get_user_by_email(db, email=user.email)
-    if db_user:
+    if db_user and db_user.id != user_id:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return create_user(db=db, user=user)
-
-@router.post("/login")
-def login(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
-    db_user = get_user_by_email(db, email=user.email)
-    if not db_user:
-        raise HTTPException(status_code=400, detail="Invalid credentials")
     
-    # Verificar si la contraseña coincide
-    if not verify_password(user.password, db_user.password):
-        raise HTTPException(status_code=400, detail="Invalid credentials")
-
-    return {"message": "Login successful"}
+    return update_user(db=db, user_id=user_id, user=user)
