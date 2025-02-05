8cd3e6a87ae06decb68be134855c69f60fdd4777
Karolpineda <karolpineda@hotmail.com>
2025-02-05 02:24:14 -0500

    feat: :sparkles: Update service for update user

--- app/services/user.py
+++ app/services/user.py
@@ -1,33 +1,26 @@
-from passlib.context import CryptContext
+# app/services/user.py
 from sqlalchemy.orm import Session
 from app.models.user import User
-from app.schemas.user import UserCreate
-import uuid
+from app.schemas.user import UserUpdate
 
-# Creamos el contexto para encriptar y verificar contraseñas
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# Función para hashear la contraseña
-def hash_password(password: str):
-    return pwd_context.hash(password)
-
-# Función para verificar la contraseña
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
+def get_user_by_id(db: Session, user_id: str):
+    return db.query(User).filter(User.id == user_id).first()
 
 # Función para actualizar un usuario
-def update_user(db: Session, user_id: str, user: UserCreate):
+def update_user(db: Session, user_id: str, user: UserUpdate):
     db_user = db.query(User).filter(User.id == user_id).first()
-    if not db_user:
-        raise HTTPException(status_code=404, detail="User not found")
     
-    db_user.first_name = user.first_name
-    db_user.last_name = user.last_name
-    db_user.email = user.email
-
-    if user.password:  # Si se quiere actualizar la contraseña
-        db_user.password = hash_password(user.password)
+    if not db_user:
+        return None
+    
+    # Actualizar los campos del usuario
+    db_user.first_name = user.first_name or db_user.first_name
+    db_user.last_name = user.last_name or db_user.last_name
+    db_user.email = user.email or db_user.email
+    db_user.password = user.password or db_user.password
     
     db.commit()
     db.refresh(db_user)
+    
     return db_user
